60368d024fab75fbd8986f2baec59c8e05fb8222
Denis Nikonov <nikon1624@gmail.com>
2021-10-07 19:47:26 +0500

    step 2

--- gendiff.js
+++ gendiff.js
@@ -4,7 +4,9 @@ import { Command } from 'commander';
 const program = new Command();
 program
   .version('1.0.0')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1><filepath2>')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
 
